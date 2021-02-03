e4488aea8df28c0e1100d541a098ac11ba6447ad
4nkit777 <sil.ankit08@gmail.com>
2021-02-03 14:57:02 -0500

    mobile first changes 2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import Aside from './components/Aside';
 import Main from './components/Main';
 // import Navbar from './components/Navbar';
 // import Projects from './pages/Projects';
-import backgroundImg from './assets/background-img.png';
+
 
 
 function App() {
   return (
     <>
-      {/* <div className='container'>
-        <img src={backgroundImg} alt='bg' />
-      </div> */}
+
       <div className='container'>
         {/* <Navbar /> */}
         <div className='row'>
